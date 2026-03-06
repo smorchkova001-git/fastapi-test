cbc96baf174c83b30c7a3996d010d68f4b47c605
Юлиана <uliana@MacBook-Air-Uliana-2.local>
2026-03-06 20:51:42 +0300

    Add current_user_optional

--- src/auth/users.py
+++ src/auth/users.py
@@ -40,3 +40,4 @@ auth_backend = AuthenticationBackend(
 fastapi_users = FastAPIUsers[User, uuid.UUID](get_user_manager, [auth_backend])
 
 current_active_user = fastapi_users.current_user(active=True)
+current_user_optional = fastapi_users.current_user(optional=True)
