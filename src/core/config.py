5d19b5448261edcb22c59bc5fc9ff21c433c4936
Юлиана <uliana@MacBook-Air-Uliana-2.local>
2026-03-06 20:16:43 +0300

    Stage 5: add Redis cache

--- src/core/config.py
+++ src/core/config.py
@@ -11,3 +11,6 @@ DB_NAME = os.getenv("DB_NAME")
 SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = os.getenv("REDIS_PORT", "6379")
